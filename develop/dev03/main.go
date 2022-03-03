e538b833a8296f569caaa5f42df822dd3243fad1
Ksenia Martynova <hurstcain@gmail.com>
2022-03-03 20:50:31 +0300

    Добавлен ввод названий файлов

--- develop/dev03/main.go
+++ develop/dev03/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"github.com/hurstcain/tasks_l2/develop/dev03/sorter"
+	"os"
+	"strings"
 )
 
 func main() {
-	sourceFile := "text.txt"
-	resultFile := "new.txt"
+	var sourceFile, resultFile string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Название исходного файла: ")
+	sourceFile, _ = reader.ReadString('\n')
+	fmt.Print("Название файла, куда будет записан результат: ")
+	resultFile, _ = reader.ReadString('\n')
+	sourceFile = strings.TrimSpace(sourceFile)
+	resultFile = strings.TrimSpace(resultFile)
+
 	kFlag := flag.Int("k", 0, "Номер колонки для сортировки")
 	nFlag := flag.Bool("n", false, "Сортировка по числовому значению")
 	rFlag := flag.Bool("r", false, "Сортировка в обратном порядке")
